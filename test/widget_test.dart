043908042ce07e3320f830a3f064b5c5ddfc303b
Neuron Electronics <develop.neuron@gmail.com>
2023-03-11 21:13:38 +0100

    changed project name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:longtermbets/main.dart';
+import 'package:betme/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
